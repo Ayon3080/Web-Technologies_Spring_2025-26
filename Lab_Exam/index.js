5ae8fa7a7d7ef63427e87562df3b20e8ac85c60e
Ayon Ghosh <ayon3080@gmail.com>
2026-03-13 00:02:10 +0600

    commit

--- Lab_Exam/index.js
+++ Lab_Exam/index.js
@@ -5,15 +5,46 @@ function appendToDisplay(input){
 }
 
 function clearDisplay(){
-    display.value="";
+    display.value = "";
 }
 
 function calculate(){
-    try{
-        display.value=eval(display.value);
+    try {
+        let input = display.value; 
+        let a = 0;
+        let b = 0;
+        let operator = "";
+        let result = 0;         
+     
+        if (input.includes("+")) {
+            operator = "+";
+        } else if (input.includes("-")) {
+            operator = "-";
+        } else if (input.includes("*")) {
+            operator = "*";
+        } else if (input.includes("/")) {
+            operator = "/";
+        }                         
+
+        if (operator !== "") {
+            let position = input.indexOf(operator); 
+            a = Number(input.substring(0, position)); 
+            b = Number(input.substring(position + 1)); 
+        }
+
+        if (operator === "+") {
+            result = a + b;
+        } else if (operator === "-") {
+            result = a - b;
+        } else if (operator === "*") {
+            result = a * b;
+        } else if (operator === "/") {
+            result = a / b;
+        }
+
+        display.value = result;
+        
+    } catch(error) {
+        display.value = "Error";
     }
-    catch(error){
-        display.value= "Error";
-    }
-    
 }
